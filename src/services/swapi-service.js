d08b81a1bd689acb1da5e9ea9edb2a15aa98d6a8
Maxim <Rudeshkomaximsergeevich@gmail.com>
2020-06-26 14:54:50 +0300

    update Star Wars API client

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,6 +1,9 @@
-export default class SwapiService{
+class SwapiService{
+
+    _apiBase = 'https://swapi.dev/api'
+
     async getResources(url){
-        const res = await fetch(url);
+        const res = await fetch(`${this._apiBase}${url}`);
 
         if(!res.ok){
             throw new Error(`Could not fetch ${res}`)
@@ -8,10 +11,30 @@ export default class SwapiService{
         return await res.json()
     }
 
-    getAllPeople(){
-        return this.getResources('http swapi.dev/api/people/')
+    async getAllPeople(){
+        const res =  await this.getResources('/people/');
+        return res.results
+    }
+
+    getPerson(id){
+        return this.getResources(`/people/${id}`)
+    }
+
+    async getAllStarships(){
+        const res = await this.getResources('/starships/')
+        return res.results
+    }
+
+    getStarship(id){
+        return this.getResources(`/starships/${id}`)
+    }
+
+    async getAllPlanet(){
+        const res = await this.getResources('/planets/')
+        return res.results
+    }
+
+    getPlanet(id){
+        return this.getResources(`/planets/${id}`)
     }
 }
-
-const people = new SwapiService();
-people.getAllPeople()
