b9eb72a54bc205077db7a93f0128f0d62d232d60
Maxim <Rudeshkomaximsergeevich@gmail.com>
2020-06-29 20:06:53 +0300

    added loading spinner for person-details component

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -8,18 +8,27 @@ export default class PersonDetails extends Component{
     swapiService = new SwapiService()
 
     state = {
-        person: null
+        person: null,
+        loading: false,
+        startMessage: true
     }
+
+    
 
     componentDidMount(){
         this.updatePerson()
     }
 
     componentDidUpdate(prevProps){
-        if(this.props.personId !== prevProps.personId){
+        if(this.props.personId !== prevProps.personId){   
+            this.setState({
+                startMessage:false,
+                loading:true,
+                person:null
+            })
+                     
             this.updatePerson()
         }
-        
     }
 
     
@@ -27,52 +36,54 @@ export default class PersonDetails extends Component{
         const {personId} = this.props
 
         if(!personId){
-             console.log(this.state + '0')
             return 
         }
-
+        this.loading = false
         this.swapiService
             .getPerson(personId)
             .then(person => {
                 this.setState({
                     person
                 })
+                
             })
         }
 
 
     render(){
-        if(!this.state.person){
-            return (
-                <span className = 'choose-character'>Select you favorite character</span>
-            )
+
+         if(this.state.startMessage){
+            return <span className = 'choose-character'>Choose you favorite character</span>
+        }
+
+        if(!this.state.person || this.loading){
+            return <Spinner/>
         }
 
         const {id, name, gender, eyeColor, birthYear} = this.state.person
-
-        return(
-            <div className = 'person-details card d-flex'>
-                <img className = 'person-image'
-                     alt = {name} 
-                     src = {`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}/>
-                <div className = 'card-body'>
-                    <h4 className = 'person-name'>{name}</h4>
-                    <ul className = 'list-group list-group-flush'>
-                        <li className = 'list-group-item'>
-                            <span className = 'term'>Gender</span>
-                            <span>{gender}</span>
-                        </li>
-                        <li className = 'list-group-item'>
-                            <span className = 'term'>Birth Year</span>
-                            <span>{birthYear}</span>
-                        </li>
-                        <li className = 'list-group-item'>
-                            <span className = 'term'>Eye color</span>
-                            <span>{eyeColor}</span>
-                        </li>
-                    </ul>
+            return(
+                <div className = 'person-details card d-flex'>
+                    <img className = 'person-image'
+                         alt = {name} 
+                         src = {`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}/>
+                    <div className = 'card-body'>
+                        <h4 className = 'person-name'>{name}</h4>
+                        <ul className = 'list-group list-group-flush'>
+                            <li className = 'list-group-item'>
+                                <span className = 'term'>Gender</span>
+                                <span>{gender}</span>
+                            </li>
+                            <li className = 'list-group-item'>
+                                <span className = 'term'>Birth Year</span>
+                                <span>{birthYear}</span>
+                            </li>
+                            <li className = 'list-group-item'>
+                                <span className = 'term'>Eye color</span>
+                                <span>{eyeColor}</span>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        )
-    }
+        )        
+    }        
 }
